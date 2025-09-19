88045380282b4c2d11ff33fda3fddc49dc4c7c6f
Novuzz <isaac.xa@gmail.com>
2025-09-19 17:46:55 -0600

    Adicionei MapLibre agora

--- lib/pages/mapVisualizer.dart
+++ lib/pages/mapVisualizer.dart
@@ -1,27 +1,92 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'dart:async';
 
-class MapVisualizer extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:maplibre_gl/maplibre_gl.dart';
+
+class MapVisualizer extends StatefulWidget {
+  final String _geoJsonId = 'buildings-source';
+  final String _layerId = 'buildings-3d';
+
+  static const CameraPosition _nullIsland = CameraPosition(
+    target: LatLng(-33.852, 151.211),
+    zoom: 2,
+  );
+
   const MapVisualizer({super.key});
 
   @override
+  State<MapVisualizer> createState() => MapVisualizerState();
+}
+
+class MapVisualizerState extends State<MapVisualizer> {
+  MapLibreMapController? _mapController;
+  bool canInteractWithMap = false;
+
+  @override
   Widget build(BuildContext context) {
-    return OSMFlutter(
-      controller: MapController(
-        initPosition: GeoPoint(latitude: 47, longitude: 8)
-      ),
-      osmOption: OSMOption(
-        userTrackingOption: UserTrackingOption(
-          enableTracking: true,
-          unFollowUser: false
-        ),
-        zoomOption:  ZoomOption(
-          initZoom: 8,
-          minZoomLevel: 3,
-          maxZoomLevel: 19,
-          stepZoom: 1.0,
-        ),
+    return Expanded(
+      child: MapLibreMap(
+        styleString: "https://demotiles.maplibre.org/style.json",
+        onMapCreated: (controller) async {
+          _mapController = controller;
+          final geojson = {
+            "type": "FeatureCollection",
+            "features": [
+              {
+                "type": "Feature",
+                "properties": {"height": 60},
+                "geometry": {
+                  "type": "Polygon",
+                  "coordinates": [
+                    [
+                      [-74.0066, 40.7135],
+                      [-74.0060, 40.7135],
+                      [-74.0060, 40.7139],
+                      [-74.0066, 40.7139],
+                      [-23.548177519867036, -46.65227339052233],
+                    ],
+                  ],
+                },
+              },
+            ],
+          };
+
+          await _mapController!.addGeoJsonSource(widget._geoJsonId, geojson);
+
+          await _mapController!.addFillExtrusionLayer(
+            widget._geoJsonId,
+            widget._layerId,
+            FillExtrusionLayerProperties(
+              fillExtrusionHeight: ['get', 'height'],
+              fillExtrusionBase: 0,
+              fillExtrusionColor: '#aaaaaa',
+              fillExtrusionOpacity: 0.9,
+              // optional: vertical gradient for nicer look
+              fillExtrusionVerticalGradient: true,
+            ),
+          );
+          await controller!.animateCamera(
+            CameraUpdate.newCameraPosition(
+              CameraPosition(
+                target: LatLng(-23.548177519867036, -46.65227339052233),
+                zoom: 17.0,
+                tilt: 60.0, // pitch to see the extrusion
+                bearing: 30.0, // rotate a bit
+              ),
+            ),
+          );
+        },
+        initialCameraPosition: MapVisualizer._nullIsland,
+        onStyleLoadedCallback: () => setState(() => canInteractWithMap = true),
       ),
     );
+  }
+
+  void _moveCameraToNullIsland() {
+    if (_mapController != null) {
+      _mapController!.animateCamera(
+        CameraUpdate.newCameraPosition(MapVisualizer._nullIsland),
+      );
+    }
   }
 }
