d031f41069deeead76dca9a05fd46b2f99cc6f1b
Novuzz <isaac.xa@gmail.com>
2025-09-25 12:52:10 -0600

    Consegui adicionar os detalhes do mapa

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,24 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:maplibre_gl/maplibre_gl.dart';
 import 'package:geo_leaf/imagePicker.dart';
 import 'package:geo_leaf/pages/mapVisualizer.dart';
 
-void main()
-{
-  runApp(
-    MyApp()
-  );
+const _nullIsland = CameraPosition(target: LatLng(0, 0), zoom: 4.0);
+void main() {
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(home:  MapLibre());
+    /*MaterialApp(
       home: Scaffold(
-        body: Center(
-          
-          child: Column(
-            children: [
-              Imagepicker(),
-              MapVisualizer()
-            ],
-          )
-        ),
+        body: Center(child: Column(children: [Imagepicker(), MapVisualizer()])),
         appBar: AppBar(
           backgroundColor: Colors.green,
           title: Center(
@@ -31,14 +26,33 @@ class MyApp extends StatelessWidget {
               "Geoleaf",
               style: TextStyle(
                 color: Colors.white,
-                fontStyle: FontStyle.italic
+                fontStyle: FontStyle.italic,
               ),
             ),
-          ) 
+          ),
         ),
-        
-      )
+      ),
+    );*/
+  }
+}
+
+class MapLibre extends StatefulWidget {
+  const MapLibre({super.key});
+
+  @override
+  State<MapLibre> createState() => _MapLibreState();
+}
+
+class _MapLibreState extends State<MapLibre> {
+  final Completer<MapLibreMapController> mapController = Completer();
+  bool canInteractWithMap = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: MapVisualizer()
     );
   }
+  void _moveCameraToNullIsland() => mapController.future.then(
+      (c) => c.animateCamera(CameraUpdate.newCameraPosition(_nullIsland)));
 }
 
--- lib/pages/mapVisualizer.dart
+++ lib/pages/mapVisualizer.dart
@@ -1,10 +1,10 @@
-import 'dart:async';
-
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
 
 class MapVisualizer extends StatefulWidget {
-  final String _geoJsonId = 'buildings-source';
+  final String _geoJsonId = '43f36e14-e3f5-43c1-84c0-50a9c80dc5c7';
   final String _layerId = 'buildings-3d';
 
   static const CameraPosition _nullIsland = CameraPosition(
@@ -24,69 +24,27 @@ class MapVisualizerState extends State<MapVisualizer> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: MapLibreMap(
-        styleString: "https://demotiles.maplibre.org/style.json",
-        onMapCreated: (controller) async {
-          _mapController = controller;
-          final geojson = {
-            "type": "FeatureCollection",
-            "features": [
-              {
-                "type": "Feature",
-                "properties": {"height": 60},
-                "geometry": {
-                  "type": "Polygon",
-                  "coordinates": [
-                    [
-                      [-74.0066, 40.7135],
-                      [-74.0060, 40.7135],
-                      [-74.0060, 40.7139],
-                      [-74.0066, 40.7139],
-                      [-23.548177519867036, -46.65227339052233],
-                    ],
-                  ],
-                },
-              },
-            ],
-          };
-
-          await _mapController!.addGeoJsonSource(widget._geoJsonId, geojson);
-
-          await _mapController!.addFillExtrusionLayer(
-            widget._geoJsonId,
-            widget._layerId,
-            FillExtrusionLayerProperties(
-              fillExtrusionHeight: ['get', 'height'],
-              fillExtrusionBase: 0,
-              fillExtrusionColor: '#aaaaaa',
-              fillExtrusionOpacity: 0.9,
-              // optional: vertical gradient for nicer look
-              fillExtrusionVerticalGradient: true,
-            ),
-          );
-          await controller!.animateCamera(
-            CameraUpdate.newCameraPosition(
-              CameraPosition(
-                target: LatLng(-23.548177519867036, -46.65227339052233),
-                zoom: 17.0,
-                tilt: 60.0, // pitch to see the extrusion
-                bearing: 30.0, // rotate a bit
-              ),
-            ),
-          );
-        },
-        initialCameraPosition: MapVisualizer._nullIsland,
-        onStyleLoadedCallback: () => setState(() => canInteractWithMap = true),
-      ),
+    return MapLibreMap(
+      initialCameraPosition: MapVisualizer._nullIsland,
+      styleString: "https://tiles.openfreemap.org/styles/bright",
+      onMapCreated: (controller) async {
+        _mapController = controller;
+        await addGJson(_mapController);
+      },
+      //onStyleLoadedCallback: () => setState(() => canInteractWithMap = true),
     );
   }
 
-  void _moveCameraToNullIsland() {
-    if (_mapController != null) {
-      _mapController!.animateCamera(
-        CameraUpdate.newCameraPosition(MapVisualizer._nullIsland),
-      );
-    }
+  Future<void> addGJson(MapLibreMapController? controller) async {
+    await controller!.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          target: LatLng(-23.548177519867036, -46.65227339052233),
+          zoom: 17.0,
+          tilt: 60.0, // pitch to see the extrusion
+          bearing: 30.0, // rotate a bit
+        ),
+      ),
+    );
   }
 }
