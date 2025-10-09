f6ef420c0b57841628f296115f782af624ffbf43
Novuzz <isaac.xa@gmail.com>
2025-10-09 17:14:17 +0400

    Adicionei os nomes dos lugares, porém para isso tive que tirar o StyleString de antes

---
+++ lib/functions/numberF.dart
@@ -0,0 +1,12 @@
+List<double> getCenter(List<dynamic> points) {
+  double x = 0.0;
+  double y = 0.0;
+  int n = points.length;
+  for (var p in points) {
+    x += (p[0] as double);
+    y += (p[1] as double);
+  }
+  x /= n;
+  y /= n;
+  return [x, y];
+}
--- lib/pages/mapVisualizer.dart
+++ lib/pages/mapVisualizer.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 import 'dart:ffi';
+import 'dart:math';
 import 'dart:typed_data';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
+import 'package:geo_leaf/functions/numberF.dart';
 
 class MapVisualizer extends StatefulWidget {
   final String _geoJsonId = '43f36e14-e3f5-43c1-84c0-50a9c80dc5c7';
@@ -28,18 +30,14 @@ class MapVisualizerState extends State<MapVisualizer> {
   Widget build(BuildContext context) {
     return MapLibreMap(
       initialCameraPosition: MapVisualizer._nullIsland,
-      styleString: "https://tiles.openfreemap.org/styles/bright",
-      onMapCreated: (controller) async {
+      styleString: "https://demotiles.maplibre.org/style.json",
+      onMapCreated: (controller)  {
         _mapController = controller;
-        //await addGJson(_mapController);
       },
       onStyleLoadedCallback: () async {
         if (_mapController != null) {
           await addGJson(_mapController);
         }
-        /*
-         
-         */
       },
     );
   }
@@ -50,85 +48,42 @@ class MapVisualizerState extends State<MapVisualizer> {
     Map<String, dynamic> jsD = await json.decode(js);
 
     await controller!.addGeoJsonSource("buildings-source", jsD);
-    final byteData = await rootBundle.load("assets/icon.png");
-    await controller.addImage("iconHolder", byteData.buffer.asUint8List());
 
-    /*
-    */
-    /*
-    await _mapController?.addSymbol(
-      SymbolOptions(
-        geometry: LatLng(-23.548177519867036, -46.65227339052233),
-        iconImage: "iconHolder",
-        iconSize: 0.2,
-      ),
-    );
-    */
     await controller.addFillExtrusionLayer(
       "buildings-source",
       "buildings-3d",
       FillExtrusionLayerProperties(
-        // fillExtrusionHeight can be an expression, here we read the 'height' property
         fillExtrusionHeight: ['get', 'height'] ?? 0,
         fillExtrusionBase: 0,
         fillExtrusionColor: ['get', 'color'] ?? '#BFD738',
         fillExtrusionOpacity: 0.9,
-        // optional: vertical gradient for nicer look
         fillExtrusionVerticalGradient: true,
       ),
-      // optional: show above other layers
-      belowLayerId: null,
     );
 
-    Map<String, Object> data = {
-      "type": "FeatureCollection",
-      "features": [
+    Map<String, Object> data = {"type": "FeatureCollection", "features": [
        
       ],
     };
-/*
+    /*
 
  */
-   for (var points in jsD['features']) {
+    for (var points in jsD['features']) {
       var localPoints = points['geometry']['coordinates'][0];
-      double x = 0.0;
-      double y = 0.0;
-      int n = localPoints.length;
-      for(var p in localPoints)
-      {
-        x += (p[0] as double);
-        y += (p[1] as double);
-      }
-      x /= n;
-      y /= n;
-      var color = points['properties']['color'];
+      final center = getCenter(localPoints );
+      var properties = points['properties'];
       (data["features"] as List).add({
         "type": "Feature",
-        "properties": 
-        {
-          "color": color
-        },
+        "properties": {"name": properties['name'], "color": properties['color']},
         "geometry": {
-          "coordinates": [x, y],
+          "coordinates": center,
           "type": "Point",
         },
       });
-    
     }
-    /*
-    (data["features"] as List).add({
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "coordinates": [-46.651471612718495, -23.546917037871992],
-        "type": "Point",
-      },
-    });
-*/
-    print(data["features"]);
     await controller.addGeoJsonSource("marker-source", data);
 
-    await controller!.addCircleLayer(
+    await controller.addCircleLayer(
       "marker-source",
       "marker-layer",
       CircleLayerProperties(
@@ -138,7 +93,27 @@ class MapVisualizerState extends State<MapVisualizer> {
         circleStrokeColor: "#ffffff",
       ),
     );
-    await controller!.animateCamera(
+
+    await controller.addSymbolLayer(
+      "marker-source",
+      "points_layer",
+      SymbolLayerProperties(
+        textField: ['get', 'name'],
+
+        textSize: 14,
+        textColor: '#ffffff',
+        textHaloColor: ['get', 'color'],
+        textHaloWidth: 1.5,
+        textAnchor: 'top',
+        textOffset: [0, 1.5],
+        textAllowOverlap: true,
+        textFont: ['Open Sans Regular', 'Arial Unicode MS Regular'],
+      ),
+      minzoom: 18
+    );
+    /*
+    */
+    await controller.animateCamera(
       CameraUpdate.newCameraPosition(
         CameraPosition(
           target: LatLng(-23.548177519867036, -46.65227339052233),
